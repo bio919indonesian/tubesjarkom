7621a74b6b804c2a7abf4ab4fdf8e6cf4e0de9ba
Naufal Yazid Z <85513585+bio919indonesian@users.noreply.github.com>
2023-05-28 20:12:57 +0700

    serverjarkom.py

--- serverjarkom.py
+++ serverjarkom.py
@@ -1,28 +1,56 @@
-import socket
+#TUGAS BESAR JARINGAN KOMPUTER - KELOMPOK 8 - IF4501
+# EKFA EDIET HAMARA (1301213360) - HASNA RAFIDA ALYA (1301213061) - NAUFAL YAZID ZACHARY (1301213001)
 
-#server address and port
-SERVER_HOST = '127.0.0.1' #alamat lokal
-SERVER_PORT = 12345 #port yang digunakan
+import socket #mengimport socket
 
-sock_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+# Socket TCP
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Membuat objek baru dimana AF_INET: alamat berbasis pada IPv4 dan SOCK_STREAM: socket TCP
 
-sock_server.bind((SERVER_HOST, SERVER_PORT))
+# Bind alamat dan port
+server_address = ('', 8000)  #Menggunakan alamat localhost dan port 8000
+server_socket.bind(server_address) #Menghubungkan alamat IP dengan nomor port ke socket
 
-sock_server.listen()
+# Listen koneksi masuk
+server_socket.listen(1) #server menerima koneksi
 
-print("Server Ready to launch")
+print('Server berjalan di http://localhost:8000') #mencetak'Server berjalan di http://localhost:8000'
 
 while True:
-        sock_client, client_address = sock_server.accept()
+    # Menunggu koneksi masuk
+    print('Menunggu koneksi...') #menampilkan string “Menunggu koneksi…”
+    client_socket, client_address = server_socket.accept() #menerima koneksi yang akan masuk untuk membuat koneksi TCP baru dari klien 
+    #yang akan mengembalikan sock_client(Objek baru untuk mengirim dan menerima data selama terhubung) dan client_address(alamat klien)
 
-        request = sock_client.recv(1024).decode()
-        print("Dari Client :" +request)
+    try:
+        print('Menerima koneksi dari', client_address) #mencetak Menerima koneksi dari' client_address
 
-        response = "Balasan Server :" +request
-        sock_client.send(response.encode())
+        # Menerima data dari client
+        data = client_socket.recv(1024) #utuk menerima data dari soket klien (client socket) melalui koneksi jaringan dengan ukuran maksimum data sebesar 1024 byte.
+        request = data.decode('utf-8') #mengubah data yang diterima dari bentuk byte menjadi bentuk string dalam format UTF-8
+        print('Data diterima:', request) #mencetak “Data telah diterima” dari soket klien.
 
-        sock_client.close()
-#endwhile
-sock_server.close()
-                        
+        try:
+            # Buka file HTML
+            with open('index.html', 'r') as file:  #membuka file dengan nama 'index.html' dalam mode membaca ('r')
+                content = file.read() #membaca seluruh isi file yang telah dibuka dan disimpan dalam variabel content
+
+        except FileNotFoundError: #menangani kesalahan yang terjadi jika file yang diinginkan untuk dibuka tidak ditemukan
+            # Kirim respons "File Not Found"
+                response = 'HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\nFile Not Found\n\n\n\n\n\n File tidak tersedia di Server\n' 
+                # variabel ‘response’ merupakan sebuah string yang berisi respon HTTP yang akan dikirimkan sebagai tanggapan atas permintaan yang diterima oleh server
+                #respon diatas menampilkan bahwa halaman atau file yang diminta tidak ditemukan (404 Not Found)
+                client_socket.sendall(response.encode('utf-8')) 
+                #untuk mengirimkan respons HTTP yang telah dikodekan ke dalam byte melalui soket klien kepada klien yang meminta respon tersebut.
+                print(response) #menampilkan isi variable ‘response’
+
+        else:
+            # Kirim response ke client
+            response = 'HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n' + content #membangun respons HTTP yang akan dikirimkan sebagai balasan dari server.
+            client_socket.sendall(response.encode('utf-8')) #mengirimkan respons HTTP yang telah dibangun (dalam bentuk string) kepada soket klien melalui koneksi jaringan.
+
+    finally:
+        # Tutup koneksi
+        client_socket.close() #baris ini menutup soket.
+    
+# Tutup Socket Server
+server_socket.close() #proses kemudian berakhir.
