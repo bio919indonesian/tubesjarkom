cec61366ba8deae31ca578150cd4aa6a892fe430
Naufal Yazid Z <85513585+bio919indonesian@users.noreply.github.com>
2023-05-28 20:13:56 +0700

    Update clientjarkom.py

--- clientjarkom.py
+++ clientjarkom.py
@@ -1,56 +1,34 @@
 #TUGAS BESAR JARINGAN KOMPUTER - KELOMPOK 8 - IF4501
 # EKFA EDIET HAMARA (1301213360) - HASNA RAFIDA ALYA (1301213061) - NAUFAL YAZID ZACHARY (1301213001)
 
+
 import socket #mengimport socket
 
-# Socket TCP
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Membuat objek baru dimana AF_INET: alamat berbasis pada IPv4 dan SOCK_STREAM: socket TCP
+# Buat socket TCP/IP
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)#Baris ini membuat soket klien, yang disebut clientSocket. Parameter pertama menunjukkan keluarga alamat; 
+#khususnya, AF_INET menunjukkan bahwa jaringan yang mendasarinya menggunakan IPv4. Parameter kedua menunjukkan bahwa soket bertipe SOCK_DGRAM, 
+#yang berarti soket tersebut adalah soket TCP (bukan soket UDP)
 
-# Bind alamat dan port
-server_address = ('', 8000)  #Menggunakan alamat localhost dan port 8000
-server_socket.bind(server_address) #Menghubungkan alamat IP dengan nomor port ke socket
+# Tentukan alamat server dan port
+server_address = ('localhost', 8000) #menetapkan alamat server ke string ‘localhost’ dan mengatur sever port variable integer ke 8000
 
-# Listen koneksi masuk
-server_socket.listen(1) #server menerima koneksi
 
-print('Server berjalan di http://localhost:8000') #mencetak'Server berjalan di http://localhost:8000'
-
-while True:
-    # Menunggu koneksi masuk
-    print('Menunggu koneksi...') #menampilkan string “Menunggu koneksi…”
-    client_socket, client_address = server_socket.accept() #menerima koneksi yang akan masuk untuk membuat koneksi TCP baru dari klien 
-    #yang akan mengembalikan sock_client(Objek baru untuk mengirim dan menerima data selama terhubung) dan client_address(alamat klien)
-
-    try:
-        print('Menerima koneksi dari', client_address) #mencetak Menerima koneksi dari' client_address
-
-        # Menerima data dari client
-        data = client_socket.recv(1024) #utuk menerima data dari soket klien (client socket) melalui koneksi jaringan dengan ukuran maksimum data sebesar 1024 byte.
-        request = data.decode('utf-8') #mengubah data yang diterima dari bentuk byte menjadi bentuk string dalam format UTF-8
-        print('Data diterima:', request) #mencetak “Data telah diterima” dari soket klien.
-
-        try:
-            # Buka file HTML
-            with open('index.html', 'r') as file:  #membuka file dengan nama 'index.html' dalam mode membaca ('r')
-                content = file.read() #membaca seluruh isi file yang telah dibuka dan disimpan dalam variabel content
-
-        except FileNotFoundError: #menangani kesalahan yang terjadi jika file yang diinginkan untuk dibuka tidak ditemukan
-            # Kirim respons "File Not Found"
-                response = 'HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\nFile Not Found\n\n\n\n\n\n File tidak tersedia di Server\n' 
-                # variabel ‘response’ merupakan sebuah string yang berisi respon HTTP yang akan dikirimkan sebagai tanggapan atas permintaan yang diterima oleh server
-                #respon diatas menampilkan bahwa halaman atau file yang diminta tidak ditemukan (404 Not Found)
-                client_socket.sendall(response.encode('utf-8')) 
-                #untuk mengirimkan respons HTTP yang telah dikodekan ke dalam byte melalui soket klien kepada klien yang meminta respon tersebut.
-                print(response) #menampilkan isi variable ‘response’
-
-        else:
-            # Kirim response ke client
-            response = 'HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n' + content #membangun respons HTTP yang akan dikirimkan sebagai balasan dari server.
-            client_socket.sendall(response.encode('utf-8')) #mengirimkan respons HTTP yang telah dibangun (dalam bentuk string) kepada soket klien melalui koneksi jaringan.
-
-    finally:
-        # Tutup koneksi
-        client_socket.close() #baris ini menutup soket.
+try:
+    # Terhubung ke server
+    client_socket.connect(server_address) #menghubungkan soket klien (client socket) ke alamat server yang ditentukan.
+    print('Terhubung ke server', server_address)
     
-# Tutup Socket Server
-server_socket.close() #proses kemudian berakhir.
+    # Mengiirim permintaan GET ke server
+    request = 'GET / HTTP/1.1\r\nHost: localhost\r\n\r\n' #membangun permintaan (request) HTTP yang akan dikirimkan kepada soket server.
+    client_socket.sendall(request.encode('utf-8')) 
+    #mengirimkan data permintaan (request) kepada soket klien melalui koneksi jaringan setelah mengubahnya menjadi bentuk byte.
+
+    # Menerima response dari server
+    response = client_socket.recv(1024)#menerima data dari soket klien (client socket) melalui koneksi jaringan dan menyimpannya dalam variabel response.
+    print('Response diterima:') #mencetak “Respinse diterima”
+    print(response.decode('utf-8'))#mencetak data yang diterima dari soket klien dalam bentuk string dengan encoding UTF-8.
+
+finally:
+    # Menutup koneksi socket
+    client_socket.close() #Baris terakhir ini menutup soket dan, karenanya, menutup koneksi TCP antara klien dan server. 
+    #Ini menyebabkan TCP di klien mengirim pesan TCP ke TCP di server.
